8cd26f02d8bb48d7269cf73239800d85f4a78b24
pozhujun <pozhujun@163.com>
2017-06-13 11:10:10 +0800

    setContentView绘制流程分析

--- app/src/main/java/study/meetpast/com/studymodule/MainActivity.java
+++ app/src/main/java/study/meetpast/com/studymodule/MainActivity.java
@@ -1,13 +1,21 @@
 package study.meetpast.com.studymodule;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * activity .setContentView绘制流程分析
+     * 创建PhoneWindow mWindow，mWindow.setContentView
+     * 创建ViewGroup mContentParent，创建DecorView mDecor
+     * mContentParent = generateLayout(mDecor); mDecor = generateDecor(-1);
+     * mLayoutInflater.inflate(layoutResID, mContentParent); mLayoutInflater = LayoutInflater.from(context);
+     *
+     */
 }
